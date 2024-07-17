ab03a4cb5ccc9e5181e848b44d7029dea1e7289d
Vadim Vagapov <midav1903@mail.ru>
2024-07-17 21:45:55 +0500

    Добавил блок 2. Добавил задачу на Human

---
+++ src/main/java/vagapov/core/block_two/Human.java
@@ -0,0 +1,29 @@
+package vagapov.core.block_two;
+
+/**
+ *Добавьте в класс Human 4 метода:
+ * 1. getSecret()
+ * 2. getNews()
+ * 3. getExperience()
+ * 4. getGossip()
+ * которые помогут ему реализовать его желание делиться разной информацией с разными людьми.
+ * Для этого используйте с методами нужные модификаторы доступа.
+ *
+ * Добавьте в класс конструктор для человека без секретов/новостей/опыта/сплетней (новорожденный)
+ */
+public class Human {
+    private final String secret; //секретики
+    private final String news; //новости
+    private final String experience; //опыт
+    private final String gossip; //сплетни
+
+    public Human(String secret, String news, String experience, String gossip) {
+        this.secret = secret;
+        this.news = news;
+        this.experience = experience;
+        this.gossip = gossip;
+    }
+
+    // Твой код здесь
+
+}
---
+++ src/test/java/vagapov/core/block_two/HumanTest.java
@@ -0,0 +1,30 @@
+package vagapov.core.block_two;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+public class HumanTest {
+
+    @Test
+    void test() {
+        Human oldHuman = new Human("secret", "news", "experiince", "gossip");
+
+        /**
+         * Раскоментируйте данный код когда будете уверены что решили задачу в классе Human
+         */
+//        assertNotNull(oldHuman.getSecret());
+//        assertNotNull(oldHuman.getNews());
+//        assertNotNull(oldHuman.getExperience());
+//        assertNotNull(oldHuman.getGossip());
+//
+//        Human newHuman = new Human();
+//        assertNull(oldHuman.getSecret());
+//        assertNull(oldHuman.getNews());
+//        assertNull(oldHuman.getExperience());
+//        assertNull(oldHuman.getGossip());
+    }
+}
